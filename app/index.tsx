c32ba651a7645a284622978a95721447a3039daa
Adrian Botello <adrianbotellog01@gmail.com>
2024-07-02 09:39:45 -0600

    LiveDetection screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,28 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Welcome from "@/components/screens/Welcome";
-import LogIn from '@/components/screens/LogIn';
-import apptheme from '@/themes/apptheme';
-
+import LiveDetection from '@/components/camera/LiveDetection';
 
 export default function index() {
-  const Stack = createStackNavigator();
 
   return (
-    // <Stack.Navigator>
-    //   <Stack.Screen 
-    //     name="Welcome" 
-    //     component={Welcome}
-    //     options={{
-    //       headerStyle: {
-    //         height: 150,
-    //         backgroundColor: apptheme.black
-            
-    //       }
-    //     }}/>
-    //   <Stack.Screen name='LogIn' component={LogIn} />
-    // </Stack.Navigator>
-    <Welcome />
-    // <LogIn />
+    // <Welcome />
+    <LiveDetection />
   );
 }
