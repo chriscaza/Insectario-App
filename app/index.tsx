7f0837ce39b108cc3bb7f086ae926096f5e24424
Adrian Botello <adrianbotellog01@gmail.com>
2024-07-03 21:27:17 -0600

    Pull Christian, Adrian

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,20 @@
+<<<<<<< HEAD
 import PhotoDetection from "@/components/screens/camera/PhotoDetection";
+=======
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Welcome from "@/components/screens/Welcome";
+import LiveDetection from '@/components/camera/LiveDetection';
+>>>>>>> c32ba65 (LiveDetection screen)
 
 export default function index() {
 
   return (
+<<<<<<< HEAD
     <PhotoDetection />
+=======
+    // <Welcome />
+    <LiveDetection />
+>>>>>>> c32ba65 (LiveDetection screen)
   );
 }
