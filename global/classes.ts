3c298da8f1be2a6ee89c891edf188f6de2a568da
Adrian Botello <adrianbotellog01@gmail.com>
2024-07-08 22:59:20 -0600

    Ordenes de arachnida

---
+++ global/classes.ts
@@ -0,0 +1,9 @@
+export const Classes = {
+    0: 'Arachnida/Acari',
+    1: 'Arachnida/Amblypygi',
+    2: 'Arachnida/Araneae',
+    3: 'Arachnida/Opiliones',
+    4: 'Arachnida/Pseudoscorpiones',
+    5: 'Arachnida/Scorpions',
+    6: 'Arachnida/Solifugae'
+}
