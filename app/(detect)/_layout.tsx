da66832c815d718f14191c97f73b5ff879f05f46
Adrian Botello <adrianbotellog01@gmail.com>
2024-07-21 13:23:38 -0600

    Custom alert casi terminado

--- app/(detect)/_layout.tsx
+++ app/(detect)/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from 'expo-router';
 export default function DetectLayout() {
     return (
         <Stack screenOptions={{headerShown: false}}>
-            <Stack.Screen name='Data'/>
+            <Stack.Screen name='Detection'/>
         </Stack>
     );
 }
