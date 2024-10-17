7506691a4524a59eba11a681dec0af0f294ec99e
Adrian Botello <adrianbotellog01@gmail.com>
2024-10-17 16:39:55 -0600

    Mapa con marcadores

--- app/(camera-flow)/MapScreen.tsx
+++ app/(camera-flow)/MapScreen.tsx
@@ -2,34 +2,35 @@ import React, { useEffect } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import { useCameraStore } from '@/global/cameraStore';
-import FastImage from 'react-native-fast-image';
+import { Image } from 'expo-image';
+import apptheme from '@/themes/apptheme';
 
 export default function MapScreen() {
 
   const photos = [
     {
       id: 1,
-      uri: 'ph://904DBA2B-3494-4260-BC1B-2C5B3E52CBCA/L0/001',
+      uri: 'ph://CCBE1BF5-6F9E-4CDE-9F6F-0EA61CF222CC/L0/001',
       coordinates: {latitude: 19.4326, longitude: -99.1332},
     },
     {
       id: 2,
-      uri: 'ph://B558F2BB-8683-4C34-9BF2-2A70EF36548E/L0/001',
+      uri: 'ph://5AE08AA4-B09B-4F60-A0BE-228E3D549D65/L0/001',
       coordinates: {latitude: 20.6597, longitude: -103.3496},
     },
     {
       id: 3,
-      uri: 'ph://CCBE1BF5-6F9E-4CDE-9F6F-0EA61CF222CC/L0/001',
+      uri: 'ph://8A3ACAFD-1F45-4739-8919-45F04D6B6827/L0/001',
       coordinates: {latitude: 21.1619, longitude: -86.8515},
     },
     {
       id: 4,
-      uri: 'ph://95074C30-607A-4F45-BB66-E9099F1CDBA5/L0/001',
+      uri: 'ph://A6060C11-AA1E-498E-A69F-2E1C695CC70B/L0/001',
       coordinates: {latitude: 55.7558, longitude: 37.6173},
     },
     {
       id: 5,
-      uri: 'ph://52172648-7E59-407D-B3DA-2ADBD6C767AB/L0/001',
+      uri: 'ph://662089EE-9438-4350-919A-8DD382F7A98F/L0/001',
       coordinates: {latitude:40.7128, longitude: -74.0060},
     }
   ]
@@ -46,10 +47,11 @@ export default function MapScreen() {
             key={photo.id}
             coordinate={photo.coordinates}
           >
-            <FastImage 
+            <Image 
               source={{ uri: photo.uri }}
               style={styles.photoMarker}
-              resizeMode={FastImage.resizeMode.cover}
+              transition={1000}
+              contentFit='cover'
             />
           </Marker>
         ))}
@@ -69,10 +71,10 @@ const styles = StyleSheet.create({
     height: '80%',
   },
   photoMarker: {
-    width: 50,
+    width: 40,
     height: 50,
-    borderRadius: 25, // Para que las fotos se vean redondeadas
+    borderRadius: 5,
     borderWidth: 2,
-    borderColor: '#FFF',
+    borderColor: apptheme.white,
   },
 });
